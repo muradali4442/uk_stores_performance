62a65c9ce95a01f15b7f2730175ecb0fd1efc11e
Murad Ali <63498217+muradali4442@users.noreply.github.com>
2023-04-04 01:38:03 +0200

    Update Logistic_Regression_Model.py

--- Logistic_Regression_Model.py
+++ Logistic_Regression_Model.py
@@ -3,109 +3,39 @@
 
 # # Importing required packages
 
-# In[14]:
-
-
 import numpy as np
 import pandas as pd
-
-
-# In[24]:
-
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
 
-# # Importing dataset
-
-# In[15]:
-
+#Importing dataset
 
 dataset_path = "data/preprocessed.csv"
-
-
-# In[16]:
-
-
 dataset = pd.read_csv(dataset_path, index_col = False)
-
-
-# In[17]:
-
-
 dataset.columns
-
-
-# In[18]:
-
 
 data = dataset.iloc[:, :].values
 
-
-# # Extracting dependant and independant variables
-
-# In[26]:
-
+#Extracting dependant and independant variables
 
 y = data[:, -1]
-
-
-# In[27]:
-
-
 X = data[:, :-1]
-
-
-# In[28]:
-
-
 y.shape
-
-
-# In[29]:
-
-
 X.shape
 
-
-# # Performing train test split
-
-# In[32]:
-
+#Performing train test split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-# In[51]:
-
 
 classifier = LogisticRegression(random_state=42)
 classifier.fit(X_train, y_train)
 
-
-# In[52]:
-
-
 y_pred = classifier.predict(X_test)
-
-
-# In[53]:
-
 
 cm = confusion_matrix(y_test, y_pred)
 cm
 
-
-# In[54]:
-
-
 ((5 + 13) / (5+5+4+13)) * 100
-
-
-# In[ ]:
-
-
-
-
