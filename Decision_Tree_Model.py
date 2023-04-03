31a7ad1102b28e1b1badcfde9931e8786bc1627c
Murad Ali <63498217+muradali4442@users.noreply.github.com>
2023-04-04 01:36:07 +0200

    Update Decision_Tree_Model.py

--- Decision_Tree_Model.py
+++ Decision_Tree_Model.py
@@ -3,109 +3,40 @@
 
 # # Importing required packages
 
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
-
-
-# In[3]:
-
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 
 
-# # Importing dataset
-
-# In[4]:
-
+#Importing dataset
 
 dataset_path = "data/preprocessed.csv"
-
-
-# In[5]:
-
-
 dataset = pd.read_csv(dataset_path, index_col = False)
-
-
-# In[6]:
-
-
 dataset.columns
-
-
-# In[7]:
-
 
 data = dataset.iloc[:, :].values
 
-
-# # Extracting dependant and independant variables
-
-# In[8]:
-
-
+#Extracting dependant and independant variables
 y = data[:, -1]
-
-
-# In[9]:
-
-
 X = data[:, :-1]
-
-
-# In[10]:
-
-
 y.shape
-
-
-# In[11]:
-
-
 X.shape
 
-
-# # Performing train test split
-
-# In[12]:
+#Performing train test split
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
-# In[14]:
-
-
 classifier = DecisionTreeClassifier(random_state=42)
 classifier.fit(X_train, y_train)
 
-
-# In[15]:
-
-
 y_pred = classifier.predict(X_test)
-
-
-# In[16]:
-
 
 cm = confusion_matrix(y_test, y_pred)
 cm
 
-
-# In[18]:
-
-
 ((5 + 12) / (5+5+5+12)) * 100
-
-
-# In[ ]:
-
-
-
 
